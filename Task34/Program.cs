e6224cc55a7d8c0fbdb483eedc5684baf3838840
Sergey Ryadchikov <ryadchikov.sa@mail.ru>
2022-08-07 18:53:06 +0300

    add Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,39 @@
+﻿void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else
+        {
+            Console.Write(array[i]);
+            Console.WriteLine();
+        }
+    }
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+int EvenElement(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+
+Console.WriteLine("Введите размерность массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int[size];
+
+FillArray(mass);
+PrintArray(mass);
+Console.Write($"Колличество четных чисел в массиве: {EvenElement(mass)}");
